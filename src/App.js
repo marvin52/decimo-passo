c6e8a2585916ed2bac2230707ceac30b779fda06
Marvin Selau Medeiros <marvin.medeiros52@gmail.com>
2023-07-19 23:47:36 -0300

    etc

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <html>
+      <head>
+        <script src="bower_components/firebaseui/dist/firebaseui.js"></script>
+        <link type="text/css" rel="stylesheet" href="bower_components/firebaseui/dist/firebaseui.css" />
+        <script src="https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui-auth__pt_br.js"></script>
+        <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.0.2/firebase-ui-auth.css" />
+      </head>
+      <body>
+        <LandingPage />
+      </body>
+    </html>
   );
 }
 
