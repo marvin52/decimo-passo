41cd4b5fe4e8814414dece98e2811575d98d3c85
Marvin Selau Medeiros <marvin.medeiros52@gmail.com>
2023-07-20 01:47:48 -0300

    things

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap';
 
 const LandingPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    description: '',
+    role: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Simulando a URL da API
+    const apiUrl = 'http://localhost:80/addUser';
+
+    // Realizando a requisição POST usando axios
+    axios
+      .post(apiUrl, formData)
+      .then((response) => {
+        console.log('Requisição POST enviada com sucesso!', response);
+      })
+      .catch((error) => {
+        console.error('Ocorreu um erro ao enviar a requisição POST.', error);
+      });
+  };
+
   return (
     <div>
       <Navbar bg="dark" variant="dark">
@@ -16,21 +47,62 @@ const LandingPage = () => {
 
       <Container>
         <h1>Bem-vindo ao Décimo Passo Online</h1>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Digite seu email" />
-          </Form.Group>
+        <div>
+      <h1>Cadastre-se!</h1>
+        <form onSubmit={handleSubmit}>
+            <div>
+              <label>Name:</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+            </div>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" placeholder="Digite sua senha" />
-          </Form.Group>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </div>
 
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <label>Description:</label>
+              <input
+                type="text"
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <label>Role:</label>
+              <select name="role" value={formData.role} onChange={handleInputChange}>
+                <option value="">Selecione</option>
+                <option value="admin">Admin</option>
+                <option value="user">User</option>
+                <option value="helper">Helper</option>
+              </select>
+            </div>
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
       </Container>
     </div>
   );
